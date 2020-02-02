1665da7c52187cc8beaa41a28611974e4ec5ac8d
Jeffrey <jeffreytan_1996@hotmail.com>
2020-02-02 23:54:56 +0800

    Day 1 - Solved Problems 1 & 2

--- array/array_longest_non_repeat.py
+++ array/array_longest_non_repeat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# Day 1 - Problem 1
 # In[1]:
 
 
--- array/array_merge_intervals.py
+++ array/array_merge_intervals.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# Day 1 - Problem 2
 # In[5]:
 
 
@@ -12,3 +12,16 @@
 # Given [[1,3],[2,6],[5,10],[11,16],[15,18],[19,22]],
 # return [[1, 10], [11, 18], [19, 22]]
 
+a = [[1,3],[2,6],[5,10],[11,16],[15,18],[19,22]]
+
+def merge_overlap(input):
+    merged = [input[0]]
+    for current in input:
+        previous = merged[-1]
+        if current[0] <= previous[1]:
+            previous[1] = max(previous[1], current[1])
+        else:
+            merged.append(current)
+    print(merged)
+
+merge_overlap(a) # Output: [[1, 10], [11, 18], [19, 22]]
