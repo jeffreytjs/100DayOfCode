680da3cb2444c5b0659d199b3549d56496fd1baa
Jeffrey <jeffreytan_1996@hotmail.com>
2020-02-02 13:59:52 +0800

    Solved

--- array/array_longest_non_repeat.py
+++ array/array_longest_non_repeat.py
@@ -8,7 +8,7 @@
 # python best courses https://courses.tanpham.org/
 # ---------------------------------------------------------------
 # Challenge
-# 
+#
 # Given a string, find the length of the longest substring
 # without repeating characters.
 
@@ -19,3 +19,20 @@
 # Given "pwwkew", the answer is "wke", with the length of 3.
 # ---------------------------------------------------------------
 
+def longest_non_repeat(input):
+    max_length = 1
+    tokenise = list(input)
+    for i in range(len(tokenise)):
+        subseq = []
+        while (i < len(input)) and (tokenise[i] not in subseq):
+            subseq.append(tokenise[i])
+            i += 1
+        if len(subseq) > max_length:
+            max_length = len(subseq)
+    print (max_length)
+    return max_length
+
+longest_non_repeat("abcabcbb") # Output = 3
+longest_non_repeat("bbbbb") # Output = 1
+longest_non_repeat("pwwkew") # Output = 3
+longest_non_repeat("abcdgbacbegsh") # Output = 7
