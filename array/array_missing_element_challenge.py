af0ff04eae8759b1c5bde704df75454aa48e6651
Jeffrey <jeffreytan_1996@hotmail.com>
2020-02-03 11:52:22 +0800

    Day 2 - Problem 3

--- array/array_missing_element_challenge.py
+++ array/array_missing_element_challenge.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# Day 2 - Problem 3
 # In[1]:
 
 
 # Consider an array of non-negative integers.
-# A second array is formed by shuffling the elements of the first array and deleting a random element. 
+# A second array is formed by shuffling the elements of the first array and deleting a random element.
 # Given these two arrays, find which element is missing in the second array.
 # Here is an example input, the first array is shuffled and the number 5 is removed to construct the second array.
 # Input:
@@ -13,3 +13,27 @@
 # Output:
 # 5 is the missing number
 
+a = [[1,2,3,4,5,6,7],[3,7,2,1,4,6]]
+# Only handles the case when one element has been removed
+def missing_element_1(input):
+    first, second = input
+    for i in first:
+        if i not in second:
+            return str(i) + " is the missing number"
+print(missing_element_1(a)) # Output: 5 is the missing number
+
+# Extension to return >1 missing elements
+b = [[1,2,3,4,5,6,7],[3,7,1,4,6]]
+def missing_element_2(input):
+    first, second = input
+    missingNo = []
+    for i in first:
+        if (i not in missingNo) and (i not in second):
+            missingNo.append(str(i))
+    # Different phrasing for different number of missing number
+    if len(missingNo) == 2:
+        return ((' and '.join(missingNo)) + " are the missing numbers")
+    else:
+        return ((', '.join(missingNo)) + " are the missing numbers")
+
+print(missing_element_2(b)) # Output: 2 and 5 are the missing numbers
