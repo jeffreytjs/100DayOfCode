c20796efa9efbdd5fe318e0f98a1b11cde6e251e
Jeffrey <jeffreytan_1996@hotmail.com>
2020-02-05 15:01:06 +0800

    Day 4 - Problem 7

--- array/array_rotate.py
+++ array/array_rotate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# Day 4 - Problem 7
 # In[3]:
 
 
@@ -11,3 +11,21 @@
 # Try to come up as many solutions as you can,
 # there are at least 3 different ways to solve this problem.
 
+# Method 1: Using two lists
+def rotate_arr1(input, steps):
+    newList = input[len(input)-(steps%len(input)):]
+    newList += input[:len(input)-(steps%len(input))]
+    print(newList)
+    return newList
+
+# Method 2: Only one list
+def rotate_arr2(input, steps):
+    while steps>0:
+        input.insert(0,input[-1])
+        input = input[:-1]
+        steps -= 1
+    print(input)
+    return input
+
+rotate_arr1([1,2,3,4,5,6,7], 20)
+rotate_arr1([1,2,3,4,5,6,7,8,9], 20)
