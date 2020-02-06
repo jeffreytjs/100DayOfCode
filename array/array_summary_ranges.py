f998020146c58af9a4840d74cefc81abcf466c4d
Jeffrey <jeffreytan_1996@hotmail.com>
2020-02-06 13:22:37 +0800

    Day 5 - Problem 8

--- array/array_summary_ranges.py
+++ array/array_summary_ranges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# Day 5 - Problem 8
 # In[2]:
 
 
@@ -8,3 +8,25 @@
 # return the summary of its ranges.
 # For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
 
+a = [0,1,2,4,5,7]
+
+def summarise(input):
+    newList = []
+    i = 0
+    count = 0
+    string = ""
+    while i<=len(input):
+        if count == 0:
+            string += str(input[i])
+            count += 1
+        elif i == len(input):
+            newList.append(string)
+        elif(input[i+1]-input[i] != 1):
+            count = 0
+            string = string + "->" + str(input[i])
+            newList.append(string)
+            string = ""
+        i += 1
+    print(newList)
+
+summarise(a)
