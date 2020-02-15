522d0ebfb1d50d7630c615e87c37b10dd400d2ce
Jeffrey <jeffreytan_1996@hotmail.com>
2020-02-15 13:25:39 +0800

    Day 13 - Problem 16

--- condition_and_loop/count_even_odd.py
+++ condition_and_loop/count_even_odd.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# Day 13 - Problem 16
 # In[ ]:
 
 
 # Count the number of even and odd numbers from a series of numbers
-# Input 
+# Input
 # numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple
 # Output
-# Number of even numbers : 4                                                                                    
-# Number of odd numbers : 5 
+# Number of even numbers : 4
+# Number of odd numbers : 5
 
+def count_odd_even(n):
+    odd_count = 0
+    even_count = 0
+    for i in n:
+        if i % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+    print('Number of even numbers : ' + str(even_count))
+    print('Number of odd numbers : ' + str(odd_count))
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+count_odd_even(numbers)
