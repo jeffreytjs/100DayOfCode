a19546cd5c61212bfa79efb0fb40cf7684658e4b
Jeffrey <jeffreytan_1996@hotmail.com>
2020-02-15 13:25:28 +0800

    Day 12 - Problem 15

--- condition_and_loop/count_digit_letter.py
+++ condition_and_loop/count_digit_letter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# Day 12 - Problem 15
 # In[ ]:
 
 
@@ -10,6 +10,19 @@
 # Write a Python program that accepts a string and calculate the number of digits and letters
 # Sample Data : "Python 3.2"
 # Expected Output :
-# Letters 6 
+# Letters 6
 # Digits 2
 
+def count_digit_letter(string):
+    letters = 0
+    digits = 0
+    for i in string:
+        if i.isalpha():
+            letters += 1
+        elif i.isnumeric():
+            digits += 1
+    print('Letters ' + str(letters))
+    print('Digits ' + str(digits))
+
+string = "Python 3.2"
+count_digit_letter(string)
