5eb5a6112a37b71815c1e260b8269045294e1e7f
Jeffrey <jeffreytan_1996@hotmail.com>
2020-02-16 12:59:42 +0800

    Day 14 - Problem 17

--- condition_and_loop/fibonaci_generator.py
+++ condition_and_loop/fibonaci_generator.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# Day 14 - Problem 17
 # In[ ]:
 
 
 # Write a Python program to get the Fibonacci series between 0 to 50
+first = 0
+last = 50
+def find_fibo(first,last):
+    results = []
+    i = first
+    while i <= last:
+        if len(results)<=1:
+            results.append(i)
+            i += 1
+        else:
+            next_fib = results[-1] + results[-2]
+            if next_fib <= last:
+                results.append(next_fib)
+                i = next_fib
+            else:
+                print(results)
+                return results
 
+find_fibo(first,last)
