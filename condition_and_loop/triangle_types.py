aed1b2021fc2cb09c20589b5a22e53faffc0ead1
Jeffrey <jeffreytan_1996@hotmail.com>
2020-02-20 13:35:06 +0800

    Day 16-17 - Problem 19-20

--- condition_and_loop/triangle_types.py
+++ condition_and_loop/triangle_types.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# Day 16 - Problem 19
 # In[ ]:
 
 
@@ -10,3 +10,12 @@
 # A scalene triangle is a triangle that has three unequal sides.
 # An isosceles triangle is a triangle with (at least) two equal sides.
 
+def check_triangle(side1, side2, side3):
+    if side1==side2==side3:
+        print('This is an equilateral triangle!')
+    elif side1==side2 or side1==side3 or side2==side3:
+        print('This is an isosceles triangle!')
+    else:
+        print('This is a scalene triangle!')
+
+check_triangle(1,2,1)
--- condition_and_loop/vowel_or_consonal.py
+++ condition_and_loop/vowel_or_consonal.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# Day 17 - Problem 20
 # In[ ]:
 
 
 # Write a Python program to check whether an alphabet is a vowel or consonant
 
+vowel = ['a','e','i','o','u']
+
+def check_letter(n):
+    if n in vowel:
+        print('This is a vowel!')
+    else:
+        print('This is a consonant!')
