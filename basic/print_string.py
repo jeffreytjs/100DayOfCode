22f5e6e2b1253b3d1312bb4b3e6fc6cbc3f5b264
Jeffrey <jeffreytan_1996@hotmail.com>
2020-08-09 01:43:09 +0800

    Refactor codes for /basic to standardize the structure. Follows flake8 linting.

--- basic/print_string.py
+++ basic/print_string.py
@@ -1,24 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
 # Day 3 - Problem 4
-# In[ ]:
 
+# Challenge
+# Write a Python program to print the following string in a specific format.
+# Hint: Use \n and \t to manipulate the string
 
-# ---------------------------------------------------------------
-# python best courses https://courses.tanpham.org/
-# ---------------------------------------------------------------
-# Write a Python program to print the following string in a specific format (see the output). Go to the editor
-
-# Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
-
+# Examples
+# Sample String:
+"""
+Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+"""
 # Output :
+"""
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are
+"""
 
-# Twinkle, twinkle, little star,
-# 	How I wonder what you are!
-# 		Up above the world so high,
-# 		Like a diamond in the sky.
-# Twinkle, twinkle, little star,
-# 	How I wonder what you are
 
-# Use \n and \t to manipulate the string
 print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are")
