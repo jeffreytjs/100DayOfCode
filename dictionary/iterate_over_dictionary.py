31cd7b1d41d2ea4c00ed42fe2f9c3161342b9dd9
Jeffrey <jeffreytan_1996@hotmail.com>
2020-08-09 02:43:05 +0800

    Day 31 - Problem 37

--- dictionary/iterate_over_dictionary.py
+++ dictionary/iterate_over_dictionary.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 31 - Problem 37
 
-# In[ ]:
-
-
+# Challenge
 # Write a Python program to iterate over dictionaries using for loops.
 
+
+d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+for key, value in d.items():
+    print(key, '->', value)
