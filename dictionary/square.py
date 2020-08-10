c2f45e0871e0e07027bb2037c0d6df8573429dd3
Jeffrey <jeffreytan_1996@hotmail.com>
2020-08-10 15:42:04 +0800

    Day 33 - Problem 39

--- dictionary/square.py
+++ dictionary/square.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 33 - Problem 39
 
-# In[ ]:
+# Challenge
+# Write a Python script to print a dictionary where
+# 1. the keys are numbers between 1 and 15 (both included)
+# 2. the values are square of keys.
 
 
-# Write a Python script to print a dictionary where the keys are numbers between 1 and 15 (both included) and the values are square of keys.
+square_of_keys = dict(
+    [
+        (n, n ** 2) for n in range(1, 16)
+    ]
+)
 
+
+print(square_of_keys)
