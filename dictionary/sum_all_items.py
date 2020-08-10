bf3f86664647cbbb97d1bee24052de918b5f1b7a
Jeffrey <jeffreytan_1996@hotmail.com>
2020-08-10 15:42:19 +0800

    Day 34 - Problem 40

--- dictionary/sum_all_items.py
+++ dictionary/sum_all_items.py
@@ -1,10 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 34 - Problem 40
 
-# In[ ]:
-
-
+# Challenge
 # Sum all the items in a dictionary
-# Input {'data1':100,'data2':-54,'data3':247}
-# Output 293
 
+# Example
+# Input: {'data1': 100, 'data2': -54, 'data3': 247}
+# Output: 293
+
+
+def sum_all_items(dict):
+    sum = 0
+    for key in dict:
+        sum += dict[key]
+    return sum
+
+
+dict = {'data1': 100, 'data2': -54, 'data3': 247}
+print(sum_all_items(dict))
