43fce317850e8eea95c1d65d91a46458a93ef469
Jeffrey <jeffreytan_1996@hotmail.com>
2020-08-11 10:13:28 +0800

    Day 35 - Problem 41

--- file/file_size.py
+++ file/file_size.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 35 - Problem 41
 
-# In[ ]:
-
-
-# ---------------------------------------------------------------
-# python best courses https://courses.tanpham.org/
-# ---------------------------------------------------------------
+# Challenge
 # Write a Python program to get the file size of a plain file.
-# Use test.txt file at same folder
+# Hint: Use test.txt file within the same folder
 
+import os
+
+
+def get_filesize(filepath):
+    try:
+        size = str(os.path.getsize(filepath)) + ' bytes'
+        return size
+
+    except Exception:
+        print(filepath + ' is not found')
+
+
+print(get_filesize('test.txt'))
