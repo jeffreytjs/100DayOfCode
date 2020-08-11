e0a0e12212d31247464b4d8a853713d98112385a
Jeffrey <jeffreytan_1996@hotmail.com>
2020-08-11 10:13:56 +0800

    Day 36 - Problem 42

--- file/first_n_lines.py
+++ file/first_n_lines.py
@@ -1,9 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 36 - Problem 42
 
-# In[ ]:
-
-
+# Challenge
 # Write a Python program to read first n lines of a file.
-# Use test.txt file
+# Hint: Use test.txt file
 
+
+def get_first_n_lines(filepath, n):
+    with open(filepath, 'r') as infile:
+        text = [next(infile) for x in range(n)]
+    print(text)
+
+
+get_first_n_lines('test.txt', 3)
