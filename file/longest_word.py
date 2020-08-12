ce6054384dbd59ab737e3f828e5d785a36942b9b
Jeffrey <jeffreytan_1996@hotmail.com>
2020-08-12 15:01:57 +0800

    Day 37 - Problem 43

--- file/longest_word.py
+++ file/longest_word.py
@@ -1,9 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 37 - Problem 43
 
-# In[ ]:
-
-
+# Challenge
 # Write a python program to find the longest words in a file
-# Using test.txt file in same folder
+# Hint: Use test.txt file within the same folder
 
+
+def get_longest_words(filepath):
+    """
+    Keeping in mind that multiple words can have the same length,
+    1) Find the length of the longest word using max
+    2) Find all words with length equal to the longest word
+    """
+    with open(filepath, 'r') as infile:
+        all_words = infile.read().split()
+    first_longest_word = max(all_words, key=len)
+    len_of_longest_word = len(first_longest_word)
+    return [word for word in all_words if len(word) == len_of_longest_word]
+
+
+print(get_longest_words('test.txt'))
