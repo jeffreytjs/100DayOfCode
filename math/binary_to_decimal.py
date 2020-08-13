db55ea03222c07e6f2668eace1c0ec447e8c35aa
Jeffrey <jeffreytan_1996@hotmail.com>
2020-08-13 10:12:47 +0800

    Day 49&50 - Problem 55&56

--- math/binary_to_decimal.py
+++ math/binary_to_decimal.py
@@ -1,8 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 49 - Problem 55
 
-# In[ ]:
-
-
+# Challenge
 # Write a Python program to convert a binary number to decimal number.
 
+# Example
+# Input = 10010
+# Output = 18
+
+
+def bin2dec(input):
+    """
+    Convert input into a list and reverse it so we can
+    work through each position
+    """
+    result = 0
+    binary = list(str(input))[::-1]
+    expo = 0
+    for i in binary:
+        if i == '1':
+            result += 2 ** expo
+        expo += 1
+    return result
+
+
+input = 10010
+print(bin2dec(input))  # 18
--- math/flip_a_coin.py
+++ math/flip_a_coin.py
@@ -1,8 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 50 - Problem 56
 
-# In[ ]:
-
-
+# Challenge
 # Write a Python program to flip a coin 1000 times and count heads and tails.
 
+
+import random
+
+
+def coin_flips(n):
+    flip_count = {
+        'heads': 0,
+        'tails': 0
+    }
+    for i in range(n):
+        if random.randint(0, 1) == 0:
+            flip_count['heads'] += 1
+        else:
+            flip_count['tails'] += 1
+
+    return flip_count
+
+
+n = 1000
+print(coin_flips(n))
