f187bc3c4b13489ec607c81ca461774d0564829f
jeffreytjs <jeffreytan_1996@hotmail.com>
2020-08-15 21:47:47 +0800

    Revert "Refactor codes for /array to standardize the structure. Follows flake…"
    This reverts commit 86f06389f0082a8d14f1f71f3985dd910b3d09ec.

--- recursion/a_power_by_b.py
+++ recursion/a_power_by_b.py
@@ -1,17 +1,8 @@
-# Day 54 - Problem 60
+#!/usr/bin/env python
+# coding: utf-8
 
-# Challenge
+# In[ ]:
+
+
 # Write a Python program to calculate the value of 'a' to the power 'b'.
 
-# Example
-# Input: a = 4, b = 3
-# Output: 64
-
-
-def power(a, b):
-    return a ** b
-
-
-a = 4
-b = 3
-print(power(a, b))
