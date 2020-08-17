059fe3ef6f0423528c77fd0906d440feda5df642
jeffreytjs <jeffreytan_1996@hotmail.com>
2020-08-17 11:54:28 +0800

    Day 59 & 60 - Problem 65 & 66

--- regex/all_word_contain_5_chracters.py
+++ regex/all_word_contain_5_chracters.py
@@ -1,12 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 59 - Problem 65
 
-# In[ ]:
-
-
+# Challenge
 # Write a Python program to find all five characters long word in a string.
-# Input
-# 'The quick brown fox jumps over the lazy dog.'
-# Output
-# ['quick', 'brown', 'jumps']
 
+# Example
+# Input: my_string = 'The quick brown fox jumps over the lazy dog.'
+# Output: ['quick', 'brown', 'jumps']
+
+
+import re
+
+
+def regex_five_char(my_string):
+    return re.findall('\w{5}', my_string)  # noqa
+
+
+def n_char_words(my_string, n):
+    # list comprehension
+    return [word for word in my_string.split(' ') if (len(word) == n)]
+
+
+my_string = 'The quick brown fox jumps over the lazy dog.'
+print(regex_five_char(my_string))
+# n = 5
+# print(n_char_words(my_string, n))
--- regex/check_string.py
+++ regex/check_string.py
@@ -1,14 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 60 - Problem 66
 
-# In[ ]:
+# Challenge
+# Write a Python program to check that a string
+# contains only a certain set of characters (in this case a-z, A-Z and 0-9).
+
+# Example
+# Input: "ABCDEFabcdef123450", "*&%@#!}{"
+# Output: True, False
 
 
-# Write a Python program to check that a string contains only a certain set of characters (in this case a-z, A-Z and 0-9).
-# Input
-# "ABCDEFabcdef123450"
-# "*&%@#!}{"
-# Output
-# True                                                                                                          
-# False
+import re
 
+
+def check_string(my_string):
+    if re.findall('[A-Za-z0-9]', my_string) == []:
+        return False
+    return True
+
+
+my_string1 = "ABCDEFabcdef123450"
+my_string2 = "*&%@#!}{"
+print(check_string(my_string1))
+print(check_string(my_string2))
