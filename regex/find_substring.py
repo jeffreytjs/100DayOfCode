f4b4b4d19d78a2e6315bb57230c18ffb7c684396
jeffreytjs <jeffreytan_1996@hotmail.com>
2020-08-18 18:17:18 +0800

    Day 61 - Problem 67 & 68

--- regex/find_substring.py
+++ regex/find_substring.py
@@ -1,17 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 60 - Problem 66
 
-# In[ ]:
-
-
+# Challenge
 # Write a Python program to find the occurrence and position of the substrings within a string.
-# 
-# Input
+
+# Example
+# Input:
 # text = 'Python exercises, PHP exercises, C# exercises'
 # pattern = 'exercises'
-# 
-# Output
-# Found "exercises" at 7:16                                                                                     
-# Found "exercises" at 22:31                                                                                    
+# Output:
+# Found "exercises" at 7:16
+# Found "exercises" at 22:31
 # Found "exercises" at 36:45
 
+
+import re
+
+
+def find_substring(pattern, text):
+    for match in re.finditer(pattern, text):
+        print('Found "' + pattern + '" at ' + str(match.start()) + ':' + str(match.end()))
+
+
+text = 'Python exercises, PHP exercises, C# exercises'
+pattern = 'exercises'
+find_substring(pattern, text)
--- regex/find_url.py
+++ regex/find_url.py
@@ -1,14 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 61 - Problem 67
 
-# In[ ]:
-
-
+# Challenge
 # Write a Python program to find urls in a string.
-# 
-# Input
-# '<p>Contents :</p><a href="https://www.w3schools.com/html/">Python Examples</a><a href="http://github.com">Even More Examples</a>'
-# 
-# Output
-# Urls:  ['https://w3resource.com', 'http://github.com']
 
+# Example
+# Input: '<p>Contents :</p><a href="https://www.w3schools.com/html/">Python Examples</a><a href="http://github.com">Even More Examples</a>'  # noqa
+# Output: Urls:  ['https://w3resource.com/html/', 'http://github.com']
+
+
+import re
+
+
+def find_urls(text):
+    pattern = '(http|ftp|https)(:\/\/)([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?'  # noqa
+    url_parts = re.findall(pattern, text)
+    url_full = [''.join(url) for url in url_parts]
+    return 'Urls: ' + str(url_full)
+
+
+text = '<p>Contents :</p><a href="https://www.w3schools.com/html/">Python Examples</a><a href="http://github.com">Even More Examples</a>'  # noqa
+print(find_urls(text))
