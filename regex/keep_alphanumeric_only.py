c09ff8c4e51ba5c52095e53c9f23453aac6c080b
jeffreytjs <jeffreytan_1996@hotmail.com>
2020-08-19 13:43:11 +0800

    Day 62 - Problem 69

--- regex/keep_alphanumeric_only.py
+++ regex/keep_alphanumeric_only.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 62 - Problem 69
 
-# In[ ]:
-
-
+# Challenge
 # Write a Python program to remove everything except alphanumeric characters from a string.
-# Input
-# '**//Python Exercises// - 12. '
-# Output
-# PythonExercises12
 
+# Example
+# Input: text = '**//Python Exercises// - 12. '
+# Output: PythonExercises12
+
+
+import re
+
+
+def check_string(text):
+    return ''.join(re.findall('[A-Za-z0-9]', text))
+
+
+text = '**//Python Exercises// - 12. '
+print(check_string(text))
