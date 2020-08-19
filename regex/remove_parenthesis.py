84acc6f3abb451c762cdcfc444cedca07fe9f095
jeffreytjs <jeffreytan_1996@hotmail.com>
2020-08-19 13:43:23 +0800

    Day 63 - Problem 70

--- regex/remove_parenthesis.py
+++ regex/remove_parenthesis.py
@@ -1,17 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 63 - Problem 70
 
-# In[ ]:
-
-
+# Challenge
 # Write a Python program to remove the parenthesis area in a string.
-# 
-# Input
-# ["example (.com)", "w3resource", "github (.com)", "stackoverflow (.com)"]
-# 
-# Output
-# example                                                                                                       
-# w3resource                                                                                                    
-# github                                                                                                        
-# stackoverflow 
 
+# Example
+# Input: my_list = ["example (.com)", "w3resource", "github (.com)", "stackoverflow (.com)"]
+# Output:
+# example
+# w3resource
+# github
+# stackoverflow
+
+
+import re
+
+
+def remove_parenthesis(text):
+    """
+    Remove anything within the parenthesis and remove whitespaces
+    """
+    return re.sub(r'\([^)]*\)', '', text).strip()
+
+
+my_list = ["example (.com)", "w3resource", "github (.com)", "stackoverflow (.com)"]
+for text in my_list:
+    print(remove_parenthesis(text))
