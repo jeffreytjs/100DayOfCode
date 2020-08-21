6008f4fa42fdf1eceda63790f35011d07ed0a090
jeffreytjs <jeffreytan_1996@hotmail.com>
2020-08-21 09:37:35 +0800

    Day 65 - Problem 72

--- regex/remove_zero.py
+++ regex/remove_zero.py
@@ -1,12 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 65 - Problem 72
 
-# In[ ]:
-
-
+# Challenge
 # Write a Python program to remove leading zeros from an IP address.
-# Input
-# "216.08.094.196"
-# Output
-# 216.8.94.196
 
+# Example
+# Input: address = "00216.08.094.196"
+# Output: 216.8.94.196
+
+
+import re
+
+
+def remove_lead_zero(text):
+    # handle leading zeroes without '.'
+    text = re.sub('^[0]*', '', text)
+    # handle all zeroes behind '.'
+    text = re.sub('\.[0]*', '.', text)  # noqa
+    return text
+
+
+address = "00216.08.094.19600"
+print(remove_lead_zero(address))
