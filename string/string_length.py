4f2d9bdad98868e82c37e630eb84f2f661d779f4
jeffreytjs <jeffreytan_1996@hotmail.com>
2020-08-22 18:34:06 +0800

    Day 66 - Problem 73

--- string/string_length.py
+++ string/string_length.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Day 66 - Problem 73
 
-# In[ ]:
-
-
+# Challenge
 # Write a Python program to calculate the length of a string.
 
+# Example
+# Input: text = 'hippopotamus'
+# Output: 12
+
+
+def str_len(text):
+    return len(text)
+
+
+text = 'hippopotamus'
+print(str_len(text))
